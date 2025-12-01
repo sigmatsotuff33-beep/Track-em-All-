f7ed601067e0afd1d2ee4703ada3a5973132bdc6
potato924 <sigmatsotuff33@gmail.com>
2025-12-01 14:59:51 +0100

    Update shodan_menu.rb

--- ruby/shodan_menu.rb
+++ ruby/shodan_menu.rb
@@ -1,15 +1,17 @@
 require_relative "python_bridge"
 
 module ShodanMenu
-    def self.prompt
-        print "IP: "
-        ip = gets.chomp
-        print "Shodan API key: "
-        api = gets.chomp
-        
-        result = PythonBridge.call_python("shodan, "#{ip}|api}")
-            puts result
-            end
-    end
+  def self.prompt
+    print "IP: "
+    ip = gets.chomp
+
+    print "Shodan API key: "
+    api = gets.chomp
+
     
-    
+    args = "#{ip}|#{api}"
+    result = PythonBridge.call_python("shodan", args)
+
+    puts result
+  end
+end
